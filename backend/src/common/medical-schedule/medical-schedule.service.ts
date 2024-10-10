01ac49e01a85d568b2cb4c10b5f7e3b1b6a1b8a9
Duy-Vu03-02 <duyvu11122002@gmail.com>
2024-10-10 09:23:22 +0700

    update

---
+++ backend/src/common/medical-schedule/medical-schedule.service.ts
@@ -0,0 +1,10 @@
+import { MedicalScheduleModel } from './medical-schedule.interface';
+
+export class MedicalScheduleService {
+    public static bookingSchedule = async () => {
+        try {
+        } catch (err) {
+            throw err;
+        }
+    };
+}
