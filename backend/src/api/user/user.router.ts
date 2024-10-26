68250f0db234c7893a576f9b51fb29e5e38fc4b0
Duy-Vu03-02 <duyvu11122002@gmail.com>
2024-10-26 16:49:38 +0700

    up to date

--- backend/src/api/user/user.router.ts
+++ backend/src/api/user/user.router.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { UserController } from './user.controller';
 import { validate } from 'express-validation';
-import { getAllBooking, userSignup } from '@api/auth/auth.user';
+import { getAllBooking, getDetailBooking, userSignup } from '@api/auth/auth.user';
+import { UserMiddleware } from '@api/auth/auth.user.middleware';
 
 const router = express.Router();
 
@@ -9,6 +10,9 @@ router.get('/medical-services', UserController.getAllService);
 
 router.post('/sign-up', validate(userSignup, { context: true }), UserController.userSignUp);
 
-router.get('/get-all-booking', validate(getAllBooking, { context: true }), UserController.getAllBooking);
+// router.get('/get-all-booking', validate(getAllBooking, { context: true }), UserController.getAllBooking);
+router.get('/get-all-booking', UserController.getAllBooking);
+
+router.get('/get-detail-booking', validate(getDetailBooking), UserMiddleware.auth, UserController.getDetailBooking);
 
 export default router;
